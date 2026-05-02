5f8abf070df03082c32c36bc528aafe793f0e01e
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-02 12:58:06 -0300

    chore: set up spring project

---
+++ Backend/src/main/java/org/northpay_contractor_onboarding/NorthpayContractorOnboardingApplication.java
@@ -0,0 +1,13 @@
+package org.northpay_contractor_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthpayContractorOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthpayContractorOnboardingApplication.class, args);
+	}
+
+}
