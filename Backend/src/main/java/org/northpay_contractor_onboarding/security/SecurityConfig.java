5a85b8813e9a67cb222dd8aa423710c8dec8fd90
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-07 10:01:32 -0300

    feat(backend security): set initial config

--- Backend/src/main/java/org/northpay_contractor_onboarding/security/SecurityConfig.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/security/SecurityConfig.java
@@ -1,4 +1,29 @@
 package org.northpay_contractor_onboarding.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity @EnableWebSecurity
 public class SecurityConfig {
+  
+  @Bean
+  SecurityFilterChain setFilterChain(HttpSecurity http) throws Exception {
+    http.
+      authorizeHttpRequests(req ->
+        req.anyRequest().permitAll() // TODO: esto es temporal, definir las rutas según los roles
+      )
+      .sessionManagement(session ->
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Debido a que se usará JWT
+      )
+      .authenticationProvider(null)
+      .addFilterBefore(null, null);
+
+    return http.build();
+  }
 }
