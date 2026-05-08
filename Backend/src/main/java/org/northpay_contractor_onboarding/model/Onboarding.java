5981fd11fc94b88bac169d754f0cc980ad9facf7
Matias Almaraz <malmaraz@frba.utn.edu.ar>
2026-05-07 22:01:02 -0300

    feat: add classes

--- Backend/src/main/java/org/northpay_contractor_onboarding/model/Onboarding.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/model/Onboarding.java
@@ -1,5 +1,12 @@
 package org.northpay_contractor_onboarding.model;
 
+import java.time.LocalDateTime;
+
 public class Onboarding {
+  private Long id;
+  private Contractor contractor;
+  private int currentStep;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt; 
 
 }
