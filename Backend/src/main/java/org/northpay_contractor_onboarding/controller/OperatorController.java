5534cb465cb4d1753b57191847ef6585a42b7832
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-11 22:44:48 -0300

    fix()operators): change some annotations in controller

--- Backend/src/main/java/org/northpay_contractor_onboarding/controller/OperatorController.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/controller/OperatorController.java
@@ -10,15 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -30,12 +29,12 @@ public class OperatorController {
   private final OperatorsService operatorsService;
 
   @GetMapping()
-  public ResponseEntity<List<OperatorDTO>> getAll(@RequestParam String param) {
+  public ResponseEntity<List<OperatorDTO>> getAll() {
     return new ResponseEntity<>(operatorsService.getAll(), HttpStatus.OK);
   }
   
   @GetMapping("/{email}")
-  public ResponseEntity<OperatorDTO> getByEmail(@NotBlank @PathVariable String email) {
+  public ResponseEntity<OperatorDTO> getByEmail(@Email @PathVariable String email) {
     return new ResponseEntity<>(operatorsService.getByEmail(email), HttpStatus.OK);
   }
   
