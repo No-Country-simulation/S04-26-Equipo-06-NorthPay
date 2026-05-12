37e3c2419353c737847c7d38a0130ae5c7490023
Matias Almaraz <malmaraz@frba.utn.edu.ar>
2026-05-11 22:45:37 -0300

    fix: Onboarding class

--- Backend/src/main/java/org/northpay_contractor_onboarding/model/Onboarding.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/model/Onboarding.java
@@ -1,7 +1,31 @@
 package org.northpay_contractor_onboarding.model;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.northpay_contractor_onboarding.enums.OnboardingStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Onboarding {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
