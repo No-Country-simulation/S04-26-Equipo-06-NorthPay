d21794e62ed8c81c18ca86f2741c5acf155505dc
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-13 14:13:47 -0300

    feat(invToken): rebuild service with register and login methods

--- Backend/src/main/java/org/northpay_contractor_onboarding/service/interfaces/IInvitationTokenService.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/service/interfaces/IInvitationTokenService.java
@@ -4,19 +4,22 @@ import java.util.List;
 import java.util.UUID;
 
 import org.northpay_contractor_onboarding.dto.InvitationTokenDTO;
+import org.northpay_contractor_onboarding.dto.authentication.InvTokenContractorSignUp;
+import org.northpay_contractor_onboarding.dto.jwt.TokenDTO;
 import org.northpay_contractor_onboarding.model.InvitationTokens;
+import org.northpay_contractor_onboarding.security.authentication.AuthenticatedUserDetails;
 
 public interface IInvitationTokenService {
   List<InvitationTokenDTO> getAll();
-  InvitationTokens create(UUID onboardingId, String contractorEmail);
+  InvitationTokens create(UUID onboardingId, String contractorEmail, AuthenticatedUserDetails authOperator);
   /**
    * 
    * @param token el token que viene desde la url
    * @return true si el token está expirado
    */
-  boolean checkInvitationTokenIsExpired(String token);
+  boolean checkInvitationTokenUrlIsExpired(String tokenUrl);
 
-  void accessToToken(String token);
+  TokenDTO useTokenForFirstTime(InvTokenContractorSignUp data);
 
-  void validateOtpForToken(String token, String code);
+  TokenDTO login(String tokenUrl, String password);
 }
