19e689c6e3824ccf752faaa0c21b7e0f1374568c
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-13 20:46:20 -0300

    feat(invTokenQuery): delete unnecessary annotation and add query to find contractor related to an invToken

--- Backend/src/main/java/org/northpay_contractor_onboarding/repository/InvitationTokenRepository.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/repository/InvitationTokenRepository.java
@@ -3,11 +3,14 @@ package org.northpay_contractor_onboarding.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.northpay_contractor_onboarding.dto.ContractorNameDTO;
 import org.northpay_contractor_onboarding.model.InvitationTokens;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
 public interface InvitationTokenRepository extends JpaRepository<InvitationTokens, UUID> {
   Optional<InvitationTokens> findByTokenUrl(String tokenUrl);
+
+  @Query("select invToken.onboarding.contractor.firstName, invToken.onboarding.contractor.lastName from InvitationTokens invToken where invToken.tokenUrl = :tokenUrl")
+  ContractorNameDTO getRelatedContractorNameByTokenUrl(String tokenUrl);
 }
