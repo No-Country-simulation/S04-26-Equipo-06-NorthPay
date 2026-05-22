8e03dcd620e2fa22d690f7822bd626be46ec62c6
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-22 11:22:29 -0300

    doc(websockets): add comments on ws config file

--- Backend/src/main/java/org/northpay_contractor_onboarding/configs/WebSocketConfig.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/configs/WebSocketConfig.java
@@ -12,15 +12,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-    registry.addEndpoint("/ws")
+    // el frontend debe configurar su conexión a http://localhost:8080/websocket antes de conectarse a cualquier canal
+    registry.addEndpoint("/websocket")
       .setAllowedOriginPatterns("*")
       .withSockJS();
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    // por convención general se usa topic para referirse a los canales a los que los receptores, ej frontend, estarán conectados
+    // prefijo que por convención general se usa topic,
+    // se usa para referirse a los canales a los que los receptores, ej frontend, estarán conectados
     registry.enableSimpleBroker("/topic");
 
     // este sería el prefijo para mandar datos al back
