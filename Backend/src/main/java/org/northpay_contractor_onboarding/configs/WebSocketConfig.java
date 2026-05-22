65632b46fd20aa6255510cc7072305f55b989b49
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2026-05-22 12:46:27 -0300

    feat(websockets): create topic routes and use it in config, add allowed origins

--- Backend/src/main/java/org/northpay_contractor_onboarding/configs/WebSocketConfig.java
+++ Backend/src/main/java/org/northpay_contractor_onboarding/configs/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package org.northpay_contractor_onboarding.configs;
 
+import org.northpay_contractor_onboarding.events.TopicRoutes;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     // el frontend debe configurar su conexión a http://localhost:8080/websocket antes de conectarse a cualquier canal
     registry.addEndpoint("/websocket")
-      .setAllowedOriginPatterns("*")
+      .setAllowedOriginPatterns("http://frontend:3000", "http://localhost:3000")
       .withSockJS();
   }
 
@@ -22,9 +23,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     // prefijo que por convención general se usa topic,
     // se usa para referirse a los canales a los que los receptores, ej frontend, estarán conectados
-    registry.enableSimpleBroker("/topic");
+    registry.enableSimpleBroker(TopicRoutes.prefix);
 
-    // este sería el prefijo para mandar datos al back
+    // este sería el prefijo para mandar datos al back desde el front (MessageMapping del controller)
     registry.setApplicationDestinationPrefixes("/api/v1/ws");
   }
 }
---
+++ Backend/src/main/java/org/northpay_contractor_onboarding/events/TopicRoutes.java
@@ -0,0 +1,7 @@
+package org.northpay_contractor_onboarding.events;
+
+public class TopicRoutes {
+  public static final String prefix = "/topic";
+  public static final String metrics = prefix + "/metrics";
+  public static final String onboardingStatus = prefix + "/onboarding-status";
+}
